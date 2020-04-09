4d737a8120344e967dee11050bbb92906d72ac45
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-09 19:24:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def list_dwarves(array)
   list_dwarves_array = []
   
   array.each_with_index do |element, index|
-    output_line = "#{index+1}. *#{element}"
+    output_line = "#{index+1}. #{element}"
     list_dwarves_array << output_line
   end
   list_dwarves_array
